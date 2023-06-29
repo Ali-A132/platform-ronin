61fcd3b9dd43b411da2140f10480be9e4b4e3b55
Ali Akbar <91800364+Ali-A132@users.noreply.github.com>
2023-06-29 18:22:41 -0400

    put sprite background for platformer and create a player character that can move, jump, and interact with environment minimally, need more work on that

--- index.js
+++ index.js
@@ -4,50 +4,117 @@ const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 const gravity = 0.4
-
-class Ronin {
-    constructor(position) {
-        this.position = position
-        this.velocity = {
-            x: 0,
-            y: 1
-        }
-        this.height = 100
-    }
-    draw() {
-        c.fillStyle = 'blue'
-        c.fillRect(this.position.x, this.position.y, 100, this.height)
-    }
-
-    // UPDATE
-    refresh()  {
-        this.draw()
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-        if(this.position.y + this.height + this.velocity.y < canvas.height)
-            this.velocity.y += gravity
-        else
-            this.velocity.y = 0
-    }
+const scaled = {
+    width: canvas.width / 4,
+    height: canvas.height / 4
 }
 
+const FloorCollisions2D = []
+for (let i = 0; i < FloorCollisions.length; i += 36) {
+    FloorCollisions2D.push(FloorCollisions.slice(i, i + 36))
+}
+
+const collisionBlocks = []
+FloorCollisions2D.forEach((row, y) => {
+    row.forEach((symbol, x) => {
+        if(symbol == 192) {
+            console.log('draw a block here!')
+            collisionBlocks.push(
+                new CollisionBlock({
+                    position: {
+                        x: x * 16,
+                        y: y * 16,
+            },
+        })
+        )
+    }
+})
+}) 
+
+const PlatformCollisions2D = []
+for (let i = 0; i < PlatformCollisions.length; i += 36) {
+    PlatformCollisions2D.push(PlatformCollisions.slice(i, i + 36))
+}
+
+const platformCollisionBlocks = []
+PlatformCollisions2D.forEach((row, y) => {
+    row.forEach((symbol, x) => {
+        if(symbol == 192) {
+            console.log('draw a block here!')
+            platformCollisionBlocks.push(
+                new CollisionBlock({
+                    position: {
+                        x: x * 16,
+                        y: y * 16,
+            },
+        })
+        )
+    }
+})
+}) 
+
 const ronin = new Ronin({
-        x: 0,
-        y: 0,
+        position: {
+            x: 100,
+            y: 300,
+        },
+        collisionBlocks,
+        imageSrc: './images/warrior/Idle.png',
+        frameRate: 10,
     }
 )
-const samurai = new Ronin({
-        x: 300,
-        y: 100,
-    }
-)
+
+
+//keys
+const movement = {
+    d: {
+        pressed: false,
+    },
+    a: {
+        pressed: false,
+    },
+    w: {
+        pressed: false,
+    },
+}
+
+const background = new Sprite({
+    position: {
+      x: 0,
+      y: 0,
+    },
+    imageSrc: './images/background-platform-dimension.png',
+})
+  
 
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'white'
     c.fillRect(0, 0, canvas.width, canvas.height)
+
+    c.save()
+    c.scale(4, 4)
+    c.translate(0, -background.image.height + scaled.height)
+    background.update()
+
+    collisionBlocks.forEach(collisionBlocks => {
+        collisionBlocks.update()
+    })
+
+    platformCollisionBlocks.forEach(block => {
+        block.update()
+    })
+
     ronin.refresh() //update
-    samurai.refresh()
+
+    ronin.velocity.x = 0
+    if(movement.d.pressed)
+        ronin.velocity.x = 5
+    else if (movement.a.pressed)
+        ronin.velocity.x = -5
+    
+    c.restore()
+
 }
 
 animate()
@@ -55,14 +122,26 @@ animate()
 window.addEventListener('keydown', (event) => {
         switch (event.key) {
             case 'd':
-                ronin.velocity.x = 1
+                movement.d.pressed = true
                 break
             case 'a':
-                ronin.velocity.x = -1
+                movement.a.pressed = true
                 break
             case 'w':
-                ronin.velocity.y = -15
+                ronin.velocity.y = -7
                 break
         }
     }
+)
+
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'd':
+            movement.d.pressed = false
+            break
+        case 'a':
+            movement.a.pressed = false
+            break
+    }
+}
 )
