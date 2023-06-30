87924cb2fe5114f84164cad69b2bb959df382e62
Ali Akbar <91800364+Ali-A132@users.noreply.github.com>
2023-06-30 01:04:25 -0400

    update character animation with enviornemnt

--- functionality/classes/Ronin.js
+++ functionality/classes/Ronin.js
@@ -1,5 +1,5 @@
 class Ronin {
-    constructor({position, collisionBlocks, imageSrc, frameRate = 1, scale = 0.6}) {
+    constructor({position, collisionBlocks, platformCollisionBlocks, imageSrc, frameRate = 1, scale = 0.6, animations}) {
         this.position = position
         this.scale = scale
         this.image = new Image()
@@ -17,6 +17,81 @@ class Ronin {
             y: 1
         }
         this.collisionBlocks = collisionBlocks
+        this.platformCollisionBlocks = platformCollisionBlocks
+        this.animations = animations
+        this.lastDirection = 'right'
+
+        for (let key in this.animations) {
+            const image = new Image()
+            image.src = this.animations[key].imageSrc
+            this.animations[key].image = image
+        }
+
+        this.camerabox = {
+            position: {
+                x: this.position.x,
+                y: this.position.y,
+            },
+            width: 200,
+            height: 80,
+        }
+    }
+
+    updateCamerabox() {
+        this.camerabox = {
+            position: {
+                x: this.position.x - 50,
+                y: this.position.y,
+            },
+            width: 200,
+            height: 80,
+        }
+    }
+
+    shouldPanCameraLeft({canvas, camera}) {
+        const cameraboxRightSide = this.camerabox.position.x + this.camerabox.width
+
+        if(cameraboxRightSide >= 576) return
+
+        if(cameraboxRightSide >= (canvas.width / 4) + Math.abs(camera.position.x)){
+            camera.position.x -= this.velocity.x
+        }
+    }
+
+    shouldPanCameraRight({canvas, camera}) {
+        if(this.camerabox.position.x + this.velocity.y <= 0) return
+        if(this.camerabox.position.x <= Math.abs(camera.position.x))
+            camera.position.x -= this.velocity.x
+    }
+
+    shouldPanCameraDown({canvas, camera}) {
+        if(this.camerabox.position.y <= 0) return
+        if(this.camerabox.position.y <= Math.abs(camera.position.y))
+            camera.position.y -= this.velocity.y
+    }
+
+    shouldPanCameraUp({canvas, camera}) {
+        if(this.camerabox.position.y + this.camerabox.height + this.velocity.y >= 432) return
+        if(this.camerabox.position.y + this.camerabox.height >= Math.abs(camera.position.y) + canvas.height / 4)
+            camera.position.y -= this.velocity.y
+    }
+
+    // TRY FOR NEXT TIME, difficult to figure out
+    // checkForHorizontalCanvasCollision() {
+    //     if (
+    //       this.hitbox.position.x + this.hitbox.width + this.velocity.x >= 576 ||
+    //       this.hitbox.position.x + this.velocity.x <= 0
+    //     ) {
+    //       this.velocity.x = 0
+    //     }
+    //   }
+
+    switchSprite(key) {
+        if(this.image === this.animations[key].image) return
+
+        this.image = this.animations[key].image
+        this.frameBuffer = this.animations[key].frameBuffer
+        this.frameRate = this.animations[key].frameRate
     }
 
     updateHitbox() {
@@ -50,22 +125,28 @@ class Ronin {
     refresh()  {
         this.updateFrames()
         this.updateHitbox()
-
-
-        c.fillStyle = 'rgba(0, 255, 0, 0.2)'
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        this.updateCamerabox()
+        // c.fillStyle = 'rgba(0, 0, 255, 0.2)'
+        // c.fillRect(this.camerabox.position.x, this.camerabox.position.y, this.camerabox.width, this.camerabox.height)
         
-        c.fillStyle = 'rgba(255, 0, 0, 0.2)'
-        c.fillRect(this.hitbox.position.x, this.hitbox.position.y, this.hitbox.width, this.hitbox.height)
+
+
+        // c.fillStyle = 'rgba(0, 255, 0, 0.2)'
+        // c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        
+        // c.fillStyle = 'rgba(255, 0, 0, 0.2)'
+        // c.fillRect(this.hitbox.position.x, this.hitbox.position.y, this.hitbox.width, this.hitbox.height)
         
         
         
         this.draw()
         this.position.x += this.velocity.x
+        this.updateHitbox()
         this.checkForHorizontalCollisions()
         this.applyGravity()
         this.updateHitbox()
         this.checkForVerticalCollisions()
+        
 
     }
 
@@ -107,9 +188,9 @@ class Ronin {
     }
 
     applyGravity() {
-        this.position.y += this.velocity.y
-        
         this.velocity.y += gravity
+        
+        this.position.y += this.velocity.y
     }
 
 
@@ -135,7 +216,23 @@ class Ronin {
                     break
                 }
             }
-        }
+        } //
+        // PLATFORM COLLISIONS
+        for(let i = 0; i < this.platformCollisionBlocks.length; i++) {
+            const platformCollisionBlock = this.platformCollisionBlocks[i]
+            if(this.hitbox.position.y + this.hitbox.height >= platformCollisionBlock.position.y && this.hitbox.position.y + this.hitbox.height <= platformCollisionBlock.position.y + platformCollisionBlock.height && this.hitbox.position.x <= platformCollisionBlock.position.x + platformCollisionBlock.width && this.hitbox.position.x + this.hitbox.width >= platformCollisionBlock.position.x){
+                if(this.velocity.y > 0) {
+                    this.velocity.y = 0
+
+                    const offset = this.hitbox.position.y - this.position.y + this.hitbox.height
+
+                    this.position.y = platformCollisionBlock.position.y - offset - 0.01
+                    break
+                }
+
+                
+            }
+        } //
     }
 
 }
